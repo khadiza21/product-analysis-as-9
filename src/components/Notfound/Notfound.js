def6a554e6b0ce44692ad7bd2fe5b68a2c44b784
khadiza21 <bibikhadiza474@gmail.com>
2022-04-03 22:31:59 +0600

    file create and update

---
+++ src/components/Notfound/Notfound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Notfound.css';
+
+const Notfound = () => {
+    return (
+        <div>
+            <h1>Notfound 404</h1>
+        </div>
+    );
+};
+
+export default Notfound;
