1900d898c00760e9645f4e1bb46de0fc6d6bb738
khadiza21 <bibikhadiza474@gmail.com>
2022-04-05 04:31:02 +0600

    10% done

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -8,12 +8,14 @@ const Main = () => {
   const [reviews, setReviews] = useProducts();
   return (
     <div className="my-5">
-      {reviews.map((review) => (
+      <h1>Customer Review (3)</h1>
+      {reviews.slice(0, 3).map((review) => (
         <Review key={review.id} review={review}></Review>
       ))}
 
-    
-      <CustomLink className="btn my-3 fw-bold" variant="btn" to="/reviews">SEE ALL REVIEWS</CustomLink>
+      <CustomLink className="btn my-3 fw-bold" variant="btn" to="/reviews">
+        SEE ALL REVIEWS
+      </CustomLink>
     </div>
   );
 };
