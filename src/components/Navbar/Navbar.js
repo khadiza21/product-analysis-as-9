169ca5fdf2e43315fa691cffb864125fbc59d602
khadiza21 <bibikhadiza474@gmail.com>
2022-04-04 19:30:00 +0600

    navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,18 @@
 import React from "react";
 import "./Navbar.css";
 import CustomLink from "../CustomLink/CustomLink";
+// import { Button } from "react-bootstrap";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-center ">
+    <div className="d-flex justify-center ">
       <CustomLink to="/">Home</CustomLink>
       <CustomLink to="/reviews">Reviews</CustomLink>
       <CustomLink to="/dashboard">Dashboard</CustomLink>
       <CustomLink to="/blogs">Blogs</CustomLink>
       <CustomLink to="/about">About</CustomLink>
+    
+     
     </div>
   );
 };
