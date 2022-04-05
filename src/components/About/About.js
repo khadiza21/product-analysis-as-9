9ee5bac52f0d70a8c6b8bd7d56d01a9dda4be7f8
khadiza21 <bibikhadiza474@gmail.com>
2022-04-05 11:44:33 +0600

    90% done

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,52 +1,68 @@
 import React from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
-
+import aboutimg from "../../image/laptop.PNG";
 import "./About.css";
 const About = () => {
   return (
     <div>
-        {/* https://nicepage.com/website-templates/preview/sustom-app-development-139937?device=desktop */}
-        {/* https://www.templatemonsterpreview.com/demo/190349.html?_gl=1*4m4nkt*_ga*MTE4NDAxNTg3Ny4xNjQ5MDg4ODY3*_ga_FTPYEGT5LY*MTY0OTA4ODg2Ni4xLjAuMTY0OTA4ODg2Ni42MA..&_ga=2.235332289.1946454262.1649088867-1184015877.1649088867 */}
-      <div>
-        <h1>WE CREATE AWESOME ANALYSIS</h1>
-        <p>tootsie roll jelly pie gummi bears gingerbread</p>
+      <div className="banner-about d-flex justify-content-center align-items-center">
+        <div className="text-center fs-5 px-3">
+          <h1 className="fw-bold mb-4">WE CREATE <span className="aw">AWESOME</span> ANALYSIS</h1>
+          <p className="fw-bold fs-5">
+            We collaborate with you to create new value through digital
+            transformation. <br /> We have hundreds of specialists at your
+            service.
+          </p>
+        </div>
       </div>
-      <div>
+
+      <div className="mt-5 pt-5">
         <Container className="container my-5 py-3">
           <Row>
-            <Col xs={12} md={6} className="my-3">
-              <img className="img-fluid rounded " src="{}" alt="" />
+            <Col xs={12} md={6} className="my-3 side">
+              <div className="px-2  text-justify side-1 m-5 d-flex justify-content-center  align-items-center">
+                <div className="px-3">
+                  <h3 className="my-2 fs-1 fw-bold">Laptop Perfection </h3>
+
+                  <p className="mt-3 fw-bold">
+                    We’re a small creative design studio based in New York,
+                    working with adventurous clients and providing Creative
+                    Concept.
+                  </p>
+                  <Button className="btn my-3 fw-bold" variant="btn">
+                    LEARN MORE
+                  </Button>
+                </div>
+              </div>
             </Col>
 
             <Col
               xs={12}
               md={6}
-              className="d-flex justify-content-center my-3 align-items-center "
+              className="d-flex justify-content-center my-3 align-items-center side-2"
             >
               <div className="px-2  text-justify">
-                <h3 className="my-2 ">AWESOME FEATURES </h3>
+                <h3 className="my-2 fs-2 fw-bold">Sample Headline </h3>
 
                 <p className="mt-3">
-                  Pie cake chupa chups lollipop. Sugar plum bonbon muffin muffin
-                  cookie applicake soufflé liquorice sweet roll. Sugar plum
-                  fruitcake tootsie roll topping. Cheesecake sugar plum jelly
-                  unerdwear.com oat cake tootsie roll chocolate cake cotton
-                  candy. Tart dragée dragée pudding donut. Oat cake soufflé
-                  dragée gingerbread cake jujubes macaroon jujubes. Croissant...
+                  We’re a small creative design studio based in New York
                 </p>
                 <Button className="btn my-3 fw-bold" variant="btn">
-                  LIVE DEMO
+                  LEARN MORE
                 </Button>
               </div>
             </Col>
           </Row>
         </Container>
 
-
         <Container className="container my-5 py-3">
           <Row>
-            <Col xs={12} md={6} className="my-3">
-              <img className="img-fluid rounded " src="{}" alt="" />
+            <Col
+              xs={12}
+              md={6}
+              className=" d-flex justify-content-center my-3 align-items-center "
+            >
+              <img className="img-fluid rounded h-75" src={aboutimg} alt="" />
             </Col>
 
             <Col
@@ -54,18 +70,17 @@ const About = () => {
               md={6}
               className="d-flex justify-content-center my-3 align-items-center "
             >
-              <div className="px-2  text-justify">
-                <h3 className="my-2 ">AWESOME FEATURES </h3>
+              <div className="px-4  text-justify">
+                <h3 className="my-2 fs-2 fw-bold pb-3 ">AWESOME FEATURES </h3>
 
                 <p className="mt-3">
-                  Pie cake chupa chups lollipop. Sugar plum bonbon muffin muffin
-                  cookie applicake soufflé liquorice sweet roll. Sugar plum
-                  fruitcake tootsie roll topping. Cheesecake sugar plum jelly
-                  unerdwear.com oat cake tootsie roll chocolate cake cotton
-                  candy. Tart dragée dragée pudding donut. Oat cake soufflé
-                  dragée gingerbread cake jujubes macaroon jujubes. Croissant...
+                  No charger was included with the computer, and the
+                  instructions are very clear that we can only use a Coolby
+                  charger--none of which are available on Amazon. Honestly, this
+                  is ridiculous. I have tried to contact the seller, but have
+                  had no response. In their literature.
                 </p>
-                <Button className="btn my-3 fw-bold" variant="btn">
+                <Button className="btn my-3  fw-bold" variant="btn">
                   LIVE DEMO
                 </Button>
               </div>
